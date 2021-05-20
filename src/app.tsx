395c9ca12a9ae0a13d39acd1ebd6099686447804
Alex <zhmurik@yandex.ru>
2021-05-20 12:02:27 -0400

    first draft

---
+++ src/app.tsx
@@ -0,0 +1,8 @@
+const App = () => (
+  <div>
+    <h1>Кто нужен?</h1>
+    <div>Находи лучших в команду</div>
+  </div>
+);
+
+export default App;
---
+++ src/common/testUtils/random.ts
@@ -0,0 +1,18 @@
+export const randomString = (len = 10, preffix = '') => {
+  const str = `${preffix}${Math.random()
+    .toString(36)
+    .slice(2)}`;
+  if (str.length >= len) {
+    return str.slice(0, len);
+  }
+  return randomString(len, str);
+};
+
+export const randomFloat = (min = 0, max = 100) =>
+  Math.random() * (max - min) + min;
+
+export const randomInt = (min = 0, max = 100) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};

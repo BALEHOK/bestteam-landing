c9c8053cedb94b270e1c0d16b3ab78612f55fea5
Alex <zhmurik@yandex.ru>
2021-06-24 13:13:12 +0300

    favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,22 @@
 module.exports = {
   siteMetadata: {
     title: "BestTeam",
+    description: "Составьте текст вакансии и наймите лучших специалистов"
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        icon: 'src/static/favicon-512x512.png',
+        "short_name": "BestTeam",
+        "name": "BestTeam - находи лучших в команду!",
+        "start_url": ".",
+        "display": "standalone",
+        "theme_color": "#000000",
+        "background_color": "#ffffff"
+      },
+    },
+  ],
 };
+
+
